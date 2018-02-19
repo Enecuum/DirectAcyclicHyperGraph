1c57bcd57fdcb595cbfe3ac7d5b7707b9741197e
Ilya Gruzdev <ilya1994s@yandex.ru>
2018-02-19 18:12:06 +0200

    Implementation of hypergraphs based on fgl by Martin Erwig

---
+++ HyperGraphExample.hs
@@ -0,0 +1,23 @@
+module HyperGraphExample where
+
+import PatriciaHyperTree
+import DirectAcyclicHyperGraph
+
+edges = [(1, "a"), (2, "b"), (3, "c"), (4, "d")] :: [LNode String]
+hyper1 = LHyper ([1,2], [3], "abc") :: (LEdge String)
+hyper2 = LHyper ([2,3], [4], "bcd") :: (LEdge String)
+hypers = [hyper1, hyper2] :: [LEdge String]
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
