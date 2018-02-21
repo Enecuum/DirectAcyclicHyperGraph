d44e2707e3ff637026242894be7a02d086d8a594
Ilya Gruzdev <ilya1994s@yandex.ru>
2018-02-21 18:03:33 +0200

    tmp

--- PatriciaHyperTree.hs
+++ PatriciaHyperTree.hs
@@ -1,161 +1,49 @@
 {-# LANGUAGE BangPatterns, CPP, ScopedTypeVariables #-}
-#if __GLASGOW_HASKELL__ >= 702
-{-# LANGUAGE DeriveGeneric #-}
-#endif
 
--- |An efficient implementation of 'Data.Graph.Inductive.Graph.Graph'
--- using big-endian patricia tree (i.e. "Data.IntMap").
---
--- This module provides the following specialised functions to gain
--- more performance, using GHC's RULES pragma:
---
--- * 'Data.Graph.Inductive.Graph.insNode'
---
--- * 'Data.Graph.Inductive.Graph.insEdge'
---
--- * 'Data.Graph.Inductive.Graph.gmap'
---
--- * 'Data.Graph.Inductive.Graph.nmap'
---
--- * 'Data.Graph.Inductive.Graph.emap'
+module HyperGraph where
 
-module PatriciaHyperTree
-    ( Gr
-    , UGr
-    )
-    where
-
-import DirectAcyclicHyperGraph
-
-import           Control.Applicative (liftA2)
-import           Data.IntMap         (IntMap)
-import qualified Data.IntMap         as IM
-import           Data.List           (foldl', sort)
-import           Data.Maybe          (fromMaybe)
-
-#if MIN_VERSION_containers (0,4,2)
-import Control.DeepSeq (NFData(..))
-#endif
-
-#if MIN_VERSION_containers(0,5,0)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM 
+import Data.Bifunctor (second)
+import Data.Maybe (fromMaybe, isJust)
 import qualified Data.IntMap.Strict as IMS
-#else
-import qualified Data.IntMap as IMS
-#endif
+import Data.List (foldl', sort)
 
-#if __GLASGOW_HASKELL__ >= 702
-import GHC.Generics (Generic)
-#endif
 
-#if MIN_VERSION_base (4,8,0)
-import Data.Bifunctor
-#else
-import Control.Arrow (second)
-#endif
+type Node         = Int
+type LNode a      = (Node, a)
 
-----------------------------------------------------------------------
--- GRAPH REPRESENTATION
-----------------------------------------------------------------------
+data LEdge b      = LEdge (Node, Node, b)
+                  | LHyper ([Node], [Node], b)
+
+--type LEdge b = (Node, Node, b)
+
+type Adj b        = [(b, Node)]
+type Context a b  = (Adj b, Node, a, Adj b)
 
 newtype Gr a b = Gr (GraphRep a b)
-#if __GLASGOW_HASKELL__ >= 702
-  deriving (Generic)
-#endif
+  deriving (Show)
 
 type GraphRep a b = IntMap (Context' a b)
 type Context' a b = (IntMap [b], a, IntMap [b])
 
-type UGr = Gr () ()
+type Decomp g a b = (MContext a b,g a b)
 
-{-
-newtype Gr a b = Gr (GraphRep a b)
-#if __GLASGOW_HASKELL__ >= 702
-  deriving (Generic)
-#endif
-
-type GraphRep a b = IntMap (Context' a b)
-type Context' a b = (IntMap [Edge b], a, IntMap [Edge b])
-
-data Edge b = Edge  (IORef b)
-            | Hyper (IORef b)
+type MContext a b = Maybe (Context a b)
 
 type UGr = Gr () ()
--}
 
-----------------------------------------------------------------------
--- CLASS INSTANCES
-----------------------------------------------------------------------
+(&) :: Context a b -> Gr a b -> Gr a b
 
-instance (Eq a, Ord b) => Eq (Gr a b) where
-  (Gr g1) == (Gr g2) = fmap sortAdj g1 == fmap sortAdj g2
-    where
-      sortAdj (p,n,s) = (fmap sort p,n,fmap sort s)
+(p, v, l, s) & (Gr g)
+  = let !g1 = IM.insert v (preds, l, succs) g
+        !(np, preds) = fromAdjCounting p
+        !(ns, succs) = fromAdjCounting s
+        !g2 = addSucc g1 v np preds
+        !g3 = addPred g2 v ns succs
+    in Gr g3
 
-instance (Show a, Show b) => Show (Gr a b) where
-  showsPrec d g = showParen (d > 10) $
-                    showString "mkGraph "
-                    . shows (labNodes g)
-                    . showString " "
-                    . shows (labEdges g)
-
-instance (Read a, Read b) => Read (Gr a b) where
-  readsPrec p = readParen (p > 10) $ \ r -> do
-    ("mkGraph", s) <- lex r
-    (ns,t) <- reads s
-    (es,u) <- reads t
-    return (mkGraph ns es, u)
-
-instance Graph Gr where
-    empty           = Gr IM.empty
-
-    isEmpty (Gr g)  = IM.null g
-
-    match           = matchGr
-
-    mkGraph vs es   = insEdges es
-                      . Gr
-                      . IM.fromList
-                      . map (second (\l -> (IM.empty,l,IM.empty)))
-                      $ vs
-
-    labNodes (Gr g) = [ (node, label)
-                            | (node, (_, label, _)) <- IM.toList g ]
-
-    noNodes   (Gr g) = IM.size g
-
-    nodeRange (Gr g) = fromMaybe (error "nodeRange of empty graph")
-                       $ liftA2 (,) (ix (IM.minViewWithKey g))
-                                    (ix (IM.maxViewWithKey g))
-      where
-        ix = fmap (fst . fst)
-    {- 
-    labEdges (Gr g) = do (node, (_, _, s)) <- IM.toList g
-                         (next, labels)    <- IM.toList s
-                         label             <- labels
-                         return (node, next, label) -}
-
-instance DynGraph Gr where
-    (p, v, l, s) & (Gr g)
-        = let !g1 = IM.insert v (preds, l, succs) g
-              !(np, preds) = fromAdjCounting p
-              !(ns, succs) = fromAdjCounting s
-              !g2 = addSucc g1 v np preds
-              !g3 = addPred g2 v ns succs
-          in Gr g3
-
-#if MIN_VERSION_containers (0,4,2)
-instance (NFData a, NFData b) => NFData (Gr a b) where
-  rnf (Gr g) = rnf g
-#endif
-
-#if MIN_VERSION_base (4,8,0)
-instance Bifunctor Gr where
-  bimap = fastNEMap
-
-  first = fastNMap
-
-  second = fastEMap
-#endif
+match = matchGr
 
 matchGr :: Node -> Gr a b -> Decomp Gr a b
 matchGr node (Gr g)
@@ -171,67 +59,22 @@ matchGr node (Gr g)
                    !g3 = clearSucc g2 node p'
                in (Just (toAdj p', node, label, toAdj s), Gr g3)
 
-----------------------------------------------------------------------
--- OVERRIDING FUNCTIONS
-----------------------------------------------------------------------
+mkGraph vs es = insEdges es
+                . Gr
+                . IM.fromList
+                . map (second (\l -> (IM.empty,l,IM.empty)))
+                $ vs
 
-{-# RULES
-      "insNode/Data.Graph.Inductive.PatriciaTree"  insNode = fastInsNode
-  #-}
-fastInsNode :: LNode a -> Gr a b -> Gr a b
-fastInsNode (v, l) (Gr g) = g' `seq` Gr g'
+insEdge :: LEdge b -> Gr a b -> Gr a b
+insEdge (LEdge (v,w,l)) g = (pr,v,la,(l,w):su) & g'
   where
-    g' = IM.insert v (IM.empty, l, IM.empty) g
+    (mcxt,g') = match v g
+    (pr,_,la,su) = fromMaybe
+                     (error ("insEdge: cannot add edge from non-existent vertex " ++ show v))
+                     mcxt
 
---{-# RULES
---      "insEdge/Data.Graph.Inductive.PatriciaTree"  insEdge = fastInsEdge
---  #-}
-{-
-fastInsEdge :: LEdge b -> Gr a b -> Gr a b
-fastInsEdge (v, w, l) (Gr g) = g2 `seq` Gr g2
-  where
-    g1 = IM.adjust addS' v g
-    g2 = IM.adjust addP' w g1
-
-    addS' (ps, l', ss) = (ps, l', IM.insertWith addLists w [l] ss)
-    addP' (ps, l', ss) = (IM.insertWith addLists v [l] ps, l', ss)
--}
-
-{-# RULES
-      "gmap/Data.Graph.Inductive.PatriciaTree"  gmap = fastGMap
-  #-}
-fastGMap :: forall a b c d. (Context a b -> Context c d) -> Gr a b -> Gr c d
-fastGMap f (Gr g) = Gr (IM.mapWithKey f' g)
-  where
-    f' :: Node -> Context' a b -> Context' c d
-    f' = ((fromContext . f) .) . toContext
-
-{-# RULES
-      "nmap/Data.Graph.Inductive.PatriciaTree"  nmap = fastNMap
-  #-}
-fastNMap :: forall a b c. (a -> c) -> Gr a b -> Gr c b
-fastNMap f (Gr g) = Gr (IM.map f' g)
-  where
-    f' :: Context' a b -> Context' c b
-    f' (ps, a, ss) = (ps, f a, ss)
-
-{-# RULES
-      "emap/Data.Graph.Inductive.PatriciaTree"  emap = fastEMap
-  #-}
-fastEMap :: forall a b c. (b -> c) -> Gr a b -> Gr a c
-fastEMap f (Gr g) = Gr (IM.map f' g)
-  where
-    f' :: Context' a b -> Context' a c
-    f' (ps, a, ss) = (IM.map (map f) ps, a, IM.map (map f) ss)
-
-{-# RULES
-      "nemap/Data.Graph.Inductive.PatriciaTree"  nemap = fastNEMap
-  #-}
-fastNEMap :: forall a b c d. (a -> c) -> (b -> d) -> Gr a b -> Gr c d
-fastNEMap fn fe (Gr g) = Gr (IM.map f g)
-  where
-    f :: Context' a b -> Context' c d
-    f (ps, a, ss) = (IM.map (map fe) ps, fn a, IM.map (map fe) ss)
+insEdges :: [LEdge b] -> Gr a b -> Gr a b
+insEdges es g = foldl' (flip insEdge) g es
 
 ----------------------------------------------------------------------
 -- UTILITIES
@@ -339,3 +182,46 @@ clearPred g v = IMS.differenceWith go g
     go :: Context' a b -> x -> Maybe (Context' a b)
     go (ps, l, ss) _ = let !ps' = IM.delete v ps
                        in Just (ps', l, ss)
+
+ns = [(1, "a"), (2, "b"), (3, "c")] 
+
+e1 = LEdge (1,2,"ab")
+e2 = LEdge (1,3,"ac")
+e3 = LEdge (2,3,"bc")
+
+es = [e1, e2]
+
+gr = mkGraph ns es
+
+inner'gr = let (Gr inner) = gr in inner
+
+--instance (Show a, Show b) => Show (Gr a b) where
+--  show (Gr xs) = show xs
+
+
+
+
+
+
+
+
+
+
+
+                      
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
