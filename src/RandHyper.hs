e3f18f4aef5a90491e2306066997642ad5d751be
Ilya Gruzdev <ilya1994s@yandex.ru>
2018-03-24 01:41:58 +0300

    test

--- src/RandHyper.hs
+++ src/RandHyper.hs
@@ -9,25 +9,25 @@ import qualified Data.IntMap as IM
 
 import DirectHyperGraph (mkGraph)
 
-nodes = zip [1..20] (take 20 $ repeat ()) 
+genHyper n k = do
+    let nodes = zip [1..n] (take 100 $ repeat ()) 
+        g     = mkGraph nodes [] :: Sub () () ()
+        h     = HGr g IM.empty
+    gen h n k
 
-g = mkGraph nodes [] :: Sub () () ()
-
-h = HGr g IM.empty
-
-gen hgr = do
-    [n1,n2] <- replicateM 2 $ randomRIO (1,10)
-    hp      <- replicateM n1 $ randomRIO (1,20)
-    hs      <- replicateM n2 $ randomRIO (1,20) 
+gen hgr n k = do
+    [n1,n2] <- replicateM 2 $ randomRIO (1,20)
+    hp      <- replicateM n1 $ randomRIO (1,n)
+    hs      <- replicateM n2 $ randomRIO (1,n) 
     let sub1  = mkGraph (zip hp (take n1 $ repeat ())) [] :: Sub () () () 
         sub2  = mkGraph (zip hs (take n2 $ repeat ())) [] :: Sub () () () 
         hyper = CDirect sub1 sub2 Block
         hgr'  = hyper +>> hgr
-    if hsize hgr' >= 10 
+    if hsize hgr' >= k
     then return hgr' 
-    else gen hgr'
+    else gen hgr' n k
 
 -- test
-hprint = (gen h) >>= hprettyPrint
+htest = (genHyper 20 10) >>= hprettyPrint
 
 
